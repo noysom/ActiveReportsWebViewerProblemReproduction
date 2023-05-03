39d035cc98756a1b26b5d6a688e421ac375cc2e6
Robert Versvik <robert.versvik@asolvi.com>
2023-05-03 10:21:30 +0200

    Add project files.

---
+++ ActiveReportsWebViewerProblemReproduction/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ActiveReportsWebViewerProblemReproduction.Default" %>
+<%@ Register TagPrefix="activereportsweb" Namespace="GrapeCity.ActiveReports.Web" Assembly="GrapeCity.ActiveReports.Web" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <activereportsweb:WebViewer id="webviewer" runat="server" Width="95%" />
+</body>
+</html>
---
+++ ActiveReportsWebViewerProblemReproduction/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using GrapeCity.ActiveReports;
+using GrapeCity.ActiveReports.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ActiveReportsWebViewerProblemReproduction
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            this.UseReporting(settings =>
+            {
+                settings.UseFileStore(new System.IO.DirectoryInfo(Server.MapPath("~")));
+                settings.UseCompression = false;
+                settings.UseCustomStore(GetReport);
+            });
+        }
+
+        private object GetReport(string reportName)
+        {
+            return new SectionReport();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
